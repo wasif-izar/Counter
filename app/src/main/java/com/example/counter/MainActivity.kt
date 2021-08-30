3e073ebf3afdad65012888555c85689722501cc0
Wasif Izar <wasifizar99@gmail.com>
2021-08-31 01:27:33 +0530

    view binding added

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -3,55 +3,73 @@ package com.example.counter
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
+import com.example.counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     //declaring text & edit text view using lateinit bcz of scope
-    private lateinit var numberText : TextView
-    private lateinit var numberInput : EditText
-    private lateinit var interval : EditText
-    private lateinit var summary : TextView
+//    private lateinit var numberText : TextView
+//    private lateinit var numberInput : EditText
+//    private lateinit var interval : EditText
+//    private lateinit var summary : TextView
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(applicationContext))
+        setContentView(binding.root)
 
         //referencing all buttons
-        val submitButton : Button = findViewById(R.id.main_act_bt_submit)
-        val randomButton : Button = findViewById(R.id.main_act_bt_random)
-        val incrementButton : Button = findViewById(R.id.main_act_bt_increment)
-        val decrementButton : Button = findViewById(R.id.main_act_bt_decrement)
+//        val submitButton : Button = findViewById(R.id.main_act_bt_submit)
+//        val randomButton : Button = findViewById(R.id.main_act_bt_random)
+//        val incrementButton : Button = findViewById(R.id.main_act_bt_increment)
+//        val decrementButton : Button = findViewById(R.id.main_act_bt_decrement)
 
         //setting clicklisteners for each button
 
-        submitButton.setOnClickListener { submitNumber() }
-        randomButton.setOnClickListener { generateRandomNumber() }
-        incrementButton.setOnClickListener { changeNumber("+") }
-        decrementButton.setOnClickListener { changeNumber("-") }
+        binding.mainActBtSubmit.setOnClickListener { submitNumber() }
+        binding.mainActBtRandom.setOnClickListener { generateRandomNumber() }
+        binding.mainActBtIncrement.setOnClickListener { changeNumber("+") }
+        binding.mainActBtDecrement.setOnClickListener { changeNumber("-") }
 
         //referencing lateinit views here
-        numberText = findViewById(R.id.main_act_tv_number)
-        numberInput = findViewById(R.id.main_act_et_input)
-        interval = findViewById(R.id.main_act_et_interval)
-        summary = findViewById(R.id.main_act_tv_summary)
+//        numberText = findViewById(R.id.main_act_tv_number)
+//        numberInput = findViewById(R.id.main_act_et_input)
+//        interval = findViewById(R.id.main_act_et_interval)
+//        summary = findViewById(R.id.main_act_tv_summary)
+
+        if(savedInstanceState != null){
+            binding.mainActTvNumber.text = savedInstanceState.getString("numberText")
+            binding.mainActEtInterval.setText(savedInstanceState.getString("interval"))
+            binding.mainActTvSummary.text = savedInstanceState.getString("summary")
+        }
 
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("numberText",binding.mainActTvNumber.text.toString())
+        outState.putString("interval",binding.mainActEtInterval.text.toString())
+        outState.putString("summary",binding.mainActTvSummary.text.toString())
+    }
 
 //    get a number from the editText and display to the screen
     private fun submitNumber(){
-        var startingNumber = numberInput.text.toString()
+        var startingNumber = binding.mainActEtInput.text.toString()
 
         if (startingNumber == ""){
             startingNumber = (-100..100).random().toString()
 
         }
-        numberText.text = startingNumber
-        numberInput.setText("")
+        binding.mainActTvNumber.text = startingNumber
+        binding.mainActEtInput.setText("")
 
         hidekeyboard()
     }
@@ -59,15 +77,15 @@ class MainActivity : AppCompatActivity() {
     private fun generateRandomNumber(){
     //random number from -100 to 100
         val randomNumber = (-100..100).random()
-        numberText.text = randomNumber.toString()
+        binding.mainActTvNumber.text = randomNumber.toString()
     }
 
 
     //increment or decrement
     private  fun changeNumber(operation : String){
         //get the current number & increment number
-        val currentNumber = numberText.text.toString().toInt()
-        var incrementValue = interval.text.toString()
+        val currentNumber = binding.mainActTvNumber.text.toString().toInt()
+        var incrementValue = binding.mainActEtInterval.text.toString()
 
         //check if increment value is blank
         if(incrementValue == "")
@@ -78,18 +96,18 @@ class MainActivity : AppCompatActivity() {
         if (operation == "+"){
             //determine new number to display
             val newNumber = currentNumber + incrementValue.toInt()
-            numberText.text = newNumber.toString()
+            binding.mainActTvNumber.text = newNumber.toString()
 
             //update summary view
-            summary.text = "$currentNumber + $incrementValue = $newNumber"
+            binding.mainActTvSummary.text = "$currentNumber + $incrementValue = $newNumber"
         }
         else{
             //determine new number to display
             val newNumber = currentNumber - incrementValue.toInt()
-            numberText.text = newNumber.toString()
+            binding.mainActTvNumber.text = newNumber.toString()
 
             //update summary view
-            summary.text = "$currentNumber - $incrementValue = $newNumber"
+            binding.mainActTvSummary.text = "$currentNumber - $incrementValue = $newNumber"
         }
         hidekeyboard()
     }
@@ -97,6 +115,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun hidekeyboard(){
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(numberText.windowToken, 0)
+        imm.hideSoftInputFromWindow(binding.mainActTvNumber.windowToken, 0)
     }
 }
